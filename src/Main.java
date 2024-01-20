2c73813b1e49f6b3627aa8d29a107bd707a6002d
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2024-01-21 00:46:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        hello();
+        welcome();
+        welcome();
+    }
+    static void hello(){
+
+        System.out.println("Hello :)");
+    }
+    static void welcome(){
+
+        System.out.println("Welcome to Java 20");
+    }
+}
